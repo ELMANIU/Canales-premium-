9f976b6e10e6d2ae15fb03e40f0d8179fe70b0f8
ELMANIU <132955458+ELMANIU@users.noreply.github.com>
2026-09-21 13:00:21 -0600

    Refactor playlist URL and simplify processing logic
    Updated the URL variable name for clarity and modified the playlist processing logic to directly replace the playlist content instead of using an if statement.

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 export default {
   async fetch(request) {
 
-    const url = "https://pub-31c3df763d1f4f2bbd2602595581aa82.r2.dev/canales/tnthd/index.m3u8";
+    const playlistUrl = "https://pub-31c3df763d1f4f2bbd2602595581aa82.r2.dev/canales/tnthd/index.m3u8";
 
-    const response = await fetch(url);
+    const response = await fetch(playlistUrl);
 
     let playlist = await response.text();
 
-    // Solo modificar playlists m3u8
-    if (playlist.includes("#EXTM3U")) {
-
-      playlist = playlist.replace(
-        "#EXTM3U",
-        "#EXTM3U\n#EXT-X-START:TIME-OFFSET=-30"
-      );
-
-    }
+    playlist = playlist.replace(
+      "#EXTM3U",
+      "#EXTM3U\n#EXT-X-START:TIME-OFFSET=-30"
+    );
 
     return new Response(playlist, {
       headers: {
         "Content-Type": "application/vnd.apple.mpegurl",
-        "Cache-Control": "no-cache, no-store, must-revalidate"
+        "Cache-Control": "no-cache"
       }
     });
-
   }
 };
