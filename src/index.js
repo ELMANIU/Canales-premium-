f726378441abd25b886414a26fb5a4d37f4959bd
ELMANIU <132955458+ELMANIU@users.noreply.github.com>
2026-09-21 13:53:50 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,79 +1,17 @@
-const R2_PLAYLIST =
-"https://pub-31c3df763d1f4f2bbd2602595581aa82.r2.dev/canales/tnthd/index.m3u8";
-
-const R2_BASE =
-"https://pub-31c3df763d1f4f2bbd2602595581aa82.r2.dev/canales/tnthd/";
-
-const DELAY = 5;
-
+const URL = "https://pub-31c3df763d1f4f2bbd2602595581aa82.r2.dev/canales/tnthd/index.m3u8";
 
 export default {
+ async fetch() {
 
-async fetch(request) {
+  const r = await fetch(URL);
 
-
-const res = await fetch(R2_PLAYLIST);
-
-let playlist = await res.text();
-
-
-let lines = playlist.trim().split("\n");
-
-let segmentPositions = [];
-
-
-// localizar segmentos .ts
-
-for(let i=0;i<lines.length;i++){
-
- if(lines[i].endsWith(".ts")){
-
-   segmentPositions.push(i);
+  return new Response(await r.text(), {
+    headers:{
+      "Content-Type":"application/x-mpegURL",
+      "Access-Control-Allow-Origin":"*",
+      "Cache-Control":"no-cache"
+    }
+  });
 
  }
-
-}
-
-
-// quitar últimos 5 segmentos
-
-let remove = segmentPositions.slice(-DELAY);
-
-
-let filtered = lines.filter((line,index)=>{
-
- return !remove.includes(index);
-
-});
-
-
-
-// convertir rutas ts relativas a absolutas
-
-filtered = filtered.map(line=>{
-
- if(line.endsWith(".ts")){
-
-   return R2_BASE + line;
-
- }
-
- return line;
-
-});
-
-
-return new Response(
- filtered.join("\n"),
- {
- headers:{
-  "Content-Type":"audio/x-mpegurl",
-  "Access-Control-Allow-Origin":"*",
-  "Cache-Control":"no-cache"
- }
-});
-
-
-}
-
 };
